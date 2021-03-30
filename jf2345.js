0c06b08e0f4a72530f4d9d323c62b532c937cce1
h455257166 <455257166@qq.com>
2021-03-30 17:37:54 +0800

    up

--- jf2345.js
+++ jf2345.js
@@ -72,7 +72,6 @@ if ($.isNode()) {
             await ProfitReport();
             await entry();
             await showMsg();
-            $.msg = '你好'
         }
       }
     if ($.isNode() && message) {
--- wplm.js
+++ wplm.js
@@ -63,11 +63,12 @@ async function start() {
     if (fs.existsSync(path)) {
         content = fs.readFileSync(path, "utf8");
     }
+    console.log(content)
    // var Time = new Date(new Date().getTime() + 8 * 60 * 60 * 1000);
     if(SEND_KEY) {
         // await notify.sendNotify("2345王牌联盟-" + new Date().toLocaleDateString(), content);
         await notify.sendNotify("2345王牌联盟", content);
-        console.log("2345王牌联盟-" + content)
+        console.log("2345王牌联盟" + content)
     }
 
     //运行完成后，删除下载的文件
